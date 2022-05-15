ac6740deeeeb4fa5f55e271e4ef7c32df66839e7
Gustavo Oliveira dos Anjos <anjos.gu@gmail.com>
2022-05-15 20:29:56 -0300

    commit inicial

---
+++ src/Hangman.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { v4 as uuidv4 } from 'uuid';
+import "./Hangman.css";
+import { randomWord } from "./words";
+import img0 from "./images/0.jpg";
+import img1 from "./images/1.jpg";
+import img2 from "./images/2.jpg";
+import img3 from "./images/3.jpg";
+import img4 from "./images/4.jpg";
+import img5 from "./images/5.jpg";
+import img6 from "./images/6.jpg";
+
+class Hangman extends Component {
+  /** by default, allow 6 guesses and use provided gallows images. */
+  static defaultProps = {
+    maxWrong: 6,
+    images: [img0, img1, img2, img3, img4, img5, img6]
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { nWrong: 0, guessed: new Set(), answer: randomWord() };
+    this.handleGuess = this.handleGuess.bind(this);
+    this.handleRestart = this.handleRestart.bind(this);
+  }
+
+  /** guessedWord: show current-state of word:
+    if guessed letters are {a,p,e}, show "app_e" for "apple"
+  */
+  guessedWord() {
+    return this.state.answer
+      .split("")
+      .map(ltr => (this.state.guessed.has(ltr) ? ltr : "_"));
+  }
+
+  /** handleGuest: handle a guessed letter:
+    - add to guessed letters
+    - if not in answer, increase number-wrong guesses
+  */
+  handleGuess(evt) {
+    let ltr = evt.target.value;
+
+    if (this.state.nWrong === this.props.maxWrong) {
+      this.restart()
+      return 
+    }
+
+    this.setState(st => ({
+      guessed: st.guessed.add(ltr),
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1)
+    }));
+  }
+
+  handleRestart() {
+    this.restart()
+  }
+
+  restart() {
+    this.setState(st => ({
+      nWrong: 0,
+      guessed: new Set(), 
+      answer: randomWord()
+    }))
+  }
+
+  /** generateButtons: return array of letter buttons to render */
+  generateButtons() {
+    return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
+      <button
+        value={ltr}
+        onClick={this.handleGuess}
+        key={uuidv4()}
+        disabled={this.state.guessed.has(ltr)}
+      >
+        {ltr}
+      </button>
+    ));
+  }
+
+  /** render: render game */
+  render() {
+    return (
+      <div className='Hangman'>
+        <h1>Hangman</h1>
+        {this.guessedWord().join("") === this.state.answer && <h2>WINNER!!!</h2>}
+        <img src={this.props.images[this.state.nWrong]} alt={this.state.nWrong + '/' + this.props.maxWrong}/>
+        <p>guessed wrong: {this.state.nWrong}</p>
+        <p className='Hangman-word'>{this.guessedWord()}</p>
+        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <br></br>
+        <button onClick={this.handleRestart}><FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon></button>
+      </div>
+    );
+  }
+}
+
+export default Hangman;
